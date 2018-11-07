ff2eac4d7cf495f078d0a255dd86ce432cfc5caf
Tyler Palmer <tyler.william.palmer@gmail.com>
2018-11-07 14:09:43 -0700

    todo list

--- exercises/to-do/index.js
+++ exercises/to-do/index.js
@@ -8,64 +8,67 @@ axios.get('https://api.vschool.io/tyler/todo/').then(function(response){
 function listTodos(arr){
     for (var i = 0; i < arr.length; i++){
     //Create new container for each Todo item
-    var todoContainer = document.createElement('div')
+        var todoContainer = document.createElement('div')
     //Add a class to that container
-    todoContainer.classList.add("todo")
+        todoContainer.classList.add("todo")
 
     //Create an html element
-    var header1 = document.createElement("h1")
-    var header2 = document.createElement("h1")
-    var header3 = document.createElement("h1")
-    
-    var image = document.createElement("img")
+        var header1 = document.createElement("h1")
+        var header2 = document.createElement("h1")
+        var header3 = document.createElement("h1")
+        
+        var image = document.createElement("img")
 
-    var title = document.createElement("h3")
-    var description = document.createElement("h3")
-    var completed = document.createElement("h3")
+        var title = document.createElement("h3")
+        var description = document.createElement("h3")
+        var completed = document.createElement("h3")
 
     //Part 3 Checkbox
 
-    var check = document.createElement("input")
-    check.type = "checkbox"
-    check.name = "checkOff"
-    
+        var check = document.createElement("input")
+        check.type = "checkbox"
+        check.name = "checkOff"
+        check.setAttribute("id", arr[i]._id)
+        
     //Put the Todo items inside of that element
 
-    header1.textContent = "Title:"
-    title.textContent = arr[i].title
+        header1.textContent = "Title:"
+        title.textContent = arr[i].title
 
-    header2.textContent = "Description:"
-    description.textContent = arr[i].description
+        header2.textContent = "Description:"
+        description.textContent = arr[i].description
 
-    if(arr[i].completed){
-        todoContainer.classList.add("finished")
-    }
+        if(arr[i].completed){
+            todoContainer.classList.add("finished")
+        }
 
-    header3.textContent = "Completed:"
-    completed.textContent = arr[i].completed
+        header3.textContent = "Completed:"
+        completed.textContent = arr[i].completed
 
-    image.src = arr[i].imgUrl
+        image.src = arr[i].imgUrl
 
     //Put element on the DOM
-    todoContainer.appendChild(header1)
-    todoContainer.appendChild(title)
+        todoContainer.appendChild(header1)
+        todoContainer.appendChild(title)
 
-    todoContainer.appendChild(header2)
-    todoContainer.appendChild(description)
+        todoContainer.appendChild(header2)
+        todoContainer.appendChild(description)
 
-    todoContainer.appendChild(header3)
-    todoContainer.appendChild(completed)
+        todoContainer.appendChild(header3)
+        todoContainer.appendChild(completed)
+        
+        todoContainer.appendChild(check)
+        
+        if (arr[i].imgUrl){
+            todoContainer.appendChild(image)
+        }else{
+            console.log("No image")
+        }
+
+        document.getElementById("list-container").appendChild(todoContainer)
+        checkOffNow(arr[i])
+        }
     
-    todoContainer.appendChild(check)
-    
-    if (arr[i].imgUrl){
-        todoContainer.appendChild(image)
-    }else{
-        console.log("No image")
-    }
-
-    document.getElementById("list-container").appendChild(todoContainer)
-    }
 }
 
 //Part 2: Post a new Todo
@@ -96,12 +99,40 @@ toDoForm.addEventListener("submit", function(event){
 
 //Part 3: Put Part 1, "Put" new value of complete or incomplete in database
 
-var checkBox = document.querySelector("input[name=checkOff]")
-
-checkBox.addEventListener('change', function(){
-    if(this.checked){
-        todoContainer.classList.add("finished")
-    }else{
-        todoContainer.classList.remove("finished")
+function checkOffNow (todo){
+    var checkBox = document.querySelectorAll("input[name=checkOff]")
+    console.log(checkBox)
+    for(var i = 0; i < checkBox.length; i++){
+        console.log(checkBox[i])
+        checkBox[i].addEventListener('change', function(event){
+            event.preventDefault()
+            var todoList2 = todo._id
+            var complete = todo.completed    
+            axios.put(`https://api.vschool.io/tyler/todo/${todoList2}`, {completed: !complete}).then(function(response){
+            })
+        })    
     }
-})
+}
+
+
+// function checkOffNow (arr){
+//     var checkBox = document.querySelectorAll("input[name=checkOff]")
+//     console.log(checkBox)
+//     var toDoContainer2 = document.getElementsByClassName("list-container")
+//     for(var i = 0; i < checkBox.length; i++){
+//         checkBox[i].addEventListener('change', function(event){
+//             event.preventDefault()
+//             console.log(arr[i])
+//             console.log(checkBox[i])
+//             if(this.checked){
+//                 var complete
+//                 axios.get('https://api.vschool.io/tyler/todo/').then(function(response){
+//                     complete = response.data.completed
+//                     axios.put('https://api.vschool.io/tyler/todo/' + this._id), {completed: !complete}
+//                 })
+//             }else{
+//                 console.log("It's not checked")
+//             }
+//         })
+//     }
+// }
