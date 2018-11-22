fe81d0f4ba37a0caf34b2298d7652cceea011f3e
Tyler <tyler.william.palmer@gmail.com>
2018-11-21 20:03:23 -0700

    11-20-18

--- exercises/superhero-events/src/App.js
+++ exercises/superhero-events/src/App.js
@@ -3,14 +3,21 @@ import Superhero from './Superhero'
 import list from './data.json'
 
 class App extends Component {
+  
+  speak = (e) =>{
+      alert(list.heroes[e].catchphrase)
+  }
   render() {
     return (
       <div>
         {
-          list.heroes.map(hero =>
+          list.heroes.map((hero, id) =>
             <Superhero  name = {hero.name}
                         image = {hero.image}
-                        catchphrase = {hero.catchphrase}/>
+                        catchphrase = {hero.catchphrase} 
+                        speak = {this.speak}
+                         id={id}/>
+                        
           )}
       </div>
     );
--- exercises/superhero-events/src/Superhero.js
+++ exercises/superhero-events/src/Superhero.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Superhero = (props) => {
-    let {name, image, catchphrase} = props
+    let {name, image, catchphrase, speak, id} = props
     return (
-        <div>
+        <div onClick={() => speak(id)}>
             <h1>Name: {name}</h1>
             <img src={image}></img>
             <h3>{catchphrase}</h3>
