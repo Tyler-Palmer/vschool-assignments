7275fc86766b2eeb3d77b94604c2c4b7d7dcad4f
Tyler <tyler.william.palmer@gmail.com>
2018-11-30 23:33:24 -0700

    personality-ibm-project

--- exercises/personality-ibm-project/src/App.js
+++ exercises/personality-ibm-project/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3')
+const personalityInsights = new PersonalityInsightsV3({
+    version: '{version}',
+    iam_apikey: '{iam_api_key}',
+    url: '{url}'
+  });
 const App = () =>{
     return(
         <div>
-            
+
         </div>
     )
 }
